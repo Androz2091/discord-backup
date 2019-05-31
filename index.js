00dee463c8b8144937d6b642f46cdc4603874805
Androz2091 <androz2091@gmail.com>
2019-05-31 17:43:07 +0200

    fix(fetch): update some path

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 /* To know more information about this npm module, go here : https://npmjs.com/package/discord-backup */
 
-const
-    util = require("util"),
-    fs = require("fs"),
-    readdir = promisify(require("fs").readdir);
+const util = require("util"),
+fs = require("fs"),
+readdir = util.promisify(require("fs").readdir);
+
+let backups = `${__dirname}/backups/`;
+
+if(!fs.existsSync(backups)){
+    fs.mkdirSync(backups);
+}
 
 module.exports = {
 
-    version = require("./package.json").version,
+    version: require("./package.json").version,
 
     /**
      * This function fetches a backyp and returns the information about it
@@ -18,23 +23,21 @@ module.exports = {
      */
     async fetch(backupID){
         return new Promise(async function(resolve, reject){
-            let files = await readdir("./backups");
-            let jsFiles = files.filter((f) => f.split(".").pop() === "js");
-            let found = false;
-            jsFiles.forEach(file => {
-                if(file === backupID){
-                    found = true;
-                    let backupInformations = require("./backups/"+file+".js");
-                    let size = fs.statSync("./backups/"+file+".js").size;
-                    resolve({
-                        ID:backupID,
-                        guildID: backupInformations.guildID,
-                        createdTimestamp: backupInformations.createdTimestamp,
-                        size: (size/1024/1024).toFixed(2)+"MB"
-                    });
-                }
-            });
-            if(!found){
+            let files = await readdir(backups); // Read "backups" directory
+            // Try to get the Json file
+            let file = files.filter((f) => f.split(".").pop() === "json").find((f) => f === `${backupID}.json`);
+            if(file){ // If the file exists
+                let backupInformations = require(`${backups}${file}`);
+                let size = fs.statSync(`${backups}${file}`).size; // Gets the size of the file using fs
+                // Returns backup informations
+                resolve({
+                    ID: backupID,
+                    guildID: backupInformations.guildID,
+                    createdTimestamp: backupInformations.createdTimestamp,
+                    size: `${(size/1024/1024).toFixed(2)}MB`
+                });
+            } else {
+                // If no backup was found, return an error message
                 reject("No backup found");
             }
         });
