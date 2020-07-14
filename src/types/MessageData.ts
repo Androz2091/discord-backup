3c3b6ea67aec1e1403931fd016022ecfbf2a6717
Tymec <tymek1rt@hotmail.com>
2020-07-14 12:45:53 +0200

    Update MessageData.ts

--- src/types/MessageData.ts
+++ src/types/MessageData.ts
@@ -1,9 +1,10 @@
-import { MessageEmbed } from "discord.js";
+import { MessageEmbed, MessageAttachment } from "discord.js";
 
 export interface MessageData {
     username: string;
     avatar?: string;
     content?: string;
     embeds?: MessageEmbed[]
+    attachments?: MessageAttachment[];
     pinned?: boolean;
 }
